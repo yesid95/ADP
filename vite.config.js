b0820787b23c92cc3887635091fe66ebc37f9435
Osmar Yesid Rincon Zorro <rincon3259@gmail.com>
2026-08-27 22:38:51 -0500

    chore(project): isolate frontend and backend checks

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
   },
   test: {
     environment: "jsdom",
-    setupFiles: "./src/test/setup.js"
+    setupFiles: "./src/test/setup.js",
+    include: ["src/**/*.{test,spec}.{js,jsx,ts,tsx}"]
   }
 });
